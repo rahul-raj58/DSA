78e890bc2cf0587b92d65d3fd9c3ffb760c0a5af
Rahul Raj <88604791+rahul-raj58@users.noreply.github.com>
2025-05-11 19:37:30 +0530

    Time: 6 ms (70.64%) | Memory: 44.9 MB (42.77%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,21 +1,43 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
+
+        // hashing --> O(n^2)  
+
+        // int n = s.length();
+        // int len = 0; 
+        // int maxLength = Integer.MIN_VALUE;
+        // if(s.length()==0)
+        //      return 0;
+        // for(int i=0; i<n; i++){
+        //     Set <Character> set = new HashSet<>();
+        //     for(int j=i; j<n; j++){
+        //         if(set.contains(s.charAt(j))){
+        //             break;
+        //         }
+        //         len = j-i+1;
+        //         maxLength = Math.max(len,maxLength);
+        //         set.add(s.charAt(j));
+        //     }  
+        // }
+        // return maxLength;
+
+        // sliding window two pointer approach --> O(n)
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        int l = 0, r = 0;
         int n = s.length();
-        int len = 0; 
-        int maxLength = Integer.MIN_VALUE;
-        if(s.length()==0)
-             return 0;
-        for(int i=0; i<n; i++){
-            Set <Character> set = new HashSet<>();
-            for(int j=i; j<n; j++){
-                if(set.contains(s.charAt(j))){
-                    break;
-                }
-                len = j-i+1;
-                maxLength = Math.max(len,maxLength);
-                set.add(s.charAt(j));
-            }  
+        int length = 0; 
+        
+        while(r<n){
+            if (map.containsKey(s.charAt(r)) && map.get(s.charAt(r)) >= l) {
+                l = map.get(s.charAt(r)) + 1;
+            }   
+            map.put(s.charAt(r), r);
+            length = Math.max(length, r-l+1);
+            r++;
         }
-        return maxLength;
+        return length;
+
     }
 }
+
