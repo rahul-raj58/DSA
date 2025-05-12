1f2b31fc830c4b28bad58144c2bfa01c5a614b6c
Rahul Raj <88604791+rahul-raj58@users.noreply.github.com>
2025-05-12 10:10:29 +0530

    Time: 9 ms (48.47%) | Memory: 44.7 MB (94.45%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -14,18 +14,52 @@ class Solution {
         // return new int[]{};
 
 
-         // HashMap
-         int n = nums.length;
-         HashMap<Integer, Integer> map = new HashMap<>();
-         for(int i=0; i<n; i++){
-            int num = nums[i];
-            int moreNeed = target - num; 
-            if(map.containsKey(moreNeed)){
-                return new int[]{map.get(moreNeed),i};
+        // HashMap TC --> O(n*log n)
+
+        //  int n = nums.length;
+        //  HashMap<Integer, Integer> map = new HashMap<>();
+        //  for(int i=0; i<n; i++){
+        //     int num = nums[i];
+        //     int moreNeed = target - num; 
+        //     if(map.containsKey(moreNeed)){
+        //         return new int[]{map.get(moreNeed),i};
+        //     }
+        //     map.put(nums[i],i);
+        //  }
+        //  return null;
+
+        // 2 - pointer TC --> O(n*log n)
+        int n = nums.length; 
+        int left = 0;
+        int right = n-1;
+        ArrayList<Pair> list = new ArrayList<>();
+        for (int i=0; i<n; i++) {
+            list.add(new Pair(nums[i], i));
+        }
+        Collections.sort(list, (o1, o2) -> o1.val - o2.val);
+        while(left<right){
+            int sum = list.get(left).val + list.get(right).val;
+            if(sum == target){
+                return new int[]{list.get(left).index, list.get(right).index};
             }
-            map.put(nums[i],i);
-         }
-         return null;
+            else if(sum < target){
+                left ++;
+            }
+            else{
+                right--;
+            }
+
+        }
+        return new int[]{};
         
     }
 }
+class Pair {
+        int val;
+        int index;
+
+        public Pair(int val, int index) {
+            this.val = val;
+            this.index = index;
+        }
+    }
