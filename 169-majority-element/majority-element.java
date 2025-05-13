05bbfb3f7d7a878773c0c6e680d2985a6a84637e
Rahul Raj <88604791+rahul-raj58@users.noreply.github.com>
2025-05-13 14:21:41 +0530

    Time: 1 ms (99.85%) | Memory: 53.7 MB (8.67%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -15,23 +15,50 @@ class Solution {
     //     return -1;
     // }
 
-    // hashMap
+        // hashMap TC--> O(n*logn)
 
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i: nums){
-            if(map.containsKey(i)){
-                map.put(i,map.get(i) + 1);
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for(int i: nums){
+        //     if(map.containsKey(i)){
+        //         map.put(i,map.get(i) + 1);
+        //     }
+        //     else{
+        //         map.put(i,1);
+        //     }
+
+        // }
+        // for(Map.Entry<Integer, Integer> it: map.entrySet()){
+        //     if(it.getValue() > (n/2)){
+        //         return it.getKey();
+        //     }
+        // }
+        // return -1;
+
+        // moore voting algorithm 
+        int count = 0; 
+        int el = 0;
+        for(int i=0; i<n; i++){
+            if(count == 0){
+                count = 1; 
+                el = nums[i];
+            }
+            else if(nums[i] == el){
+                count++;
             }
             else{
-                map.put(i,1);
+                count--;
             }
-
         }
-        for(Map.Entry<Integer, Integer> it: map.entrySet()){
-            if(it.getValue() > (n/2)){
-                return it.getKey();
+        int count1 = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i] == el){
+                count1++;
             }
+        }
+        if(count1 > n/2){
+            return el;
         }
         return -1;
+
     }
 }
