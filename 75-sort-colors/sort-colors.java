b6177f5baba9887416c6c56da351020ba302aa80
Rahul Raj <88604791+rahul-raj58@users.noreply.github.com>
2025-05-13 12:08:10 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (65.34%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,27 +1,51 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int count0 = 0; 
-        int count1 = 0; 
-        int count2 = 0; 
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] == 0){
-                count0++;
-            }
-            else if(nums[i] == 1){
-                count1++;
-            }
-            else{
-                count2++;
-            }
+    //     int count0 = 0; 
+    //     int count1 = 0; 
+    //     int count2 = 0; 
+    //     for(int i=0; i<nums.length; i++){
+    //         if(nums[i] == 0){
+    //             count0++;
+    //         }
+    //         else if(nums[i] == 1){
+    //             count1++;
+    //         }
+    //         else{
+    //             count2++;
+    //         }
+    //     }
+    //     for(int i=0; i<count0; i++){
+    //         nums[i] = 0;
+    //     }
+    //     for(int i=count0; i<count0+count1; i++){
+    //         nums[i] = 1;
+    //     }
+    //     for(int i=count0+count1; i<nums.length; i++){
+    //         nums[i] = 2;
+    //     }
+
+    // Dutch National Flag Algorithm 
+    int n = nums.length; 
+    int low = 0; 
+    int mid = 0;
+    int high = n-1;
+    while(mid <= high){
+        if(nums[mid] == 0){
+            int temp = nums[mid];
+            nums[mid] = nums[low];
+            nums[low] = temp;
+            low++;
+            mid++;
         }
-        for(int i=0; i<count0; i++){
-            nums[i] = 0;
+        else if(nums[mid] == 1){
+            mid++;
         }
-        for(int i=count0; i<count0+count1; i++){
-            nums[i] = 1;
+        else{
+            int temp = nums[mid];
+            nums[mid] = nums[high];
+            nums[high] = temp;
+            high--;
         }
-        for(int i=count0+count1; i<nums.length; i++){
-            nums[i] = 2;
-        }
+    }
     }
 }
