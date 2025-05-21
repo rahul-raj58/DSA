0c18e5ff596df29368ab2acafd9a1d49c0c4d15e
Rahul Raj <88604791+rahul-raj58@users.noreply.github.com>
2025-05-21 13:38:23 +0530

    Time: 17 ms (44.73%) | Memory: 46.3 MB (50.59%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,18 +1,32 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        int n = nums.length;
+        // int n = nums.length;
+        // int nge[] = new int[n];
+        // Arrays.fill(nge, -1);
+        // for(int i=0; i<n; i++){
+        //     for(int j=i+1; j<i+n; j++){
+        //         int ind = j%n;
+        //         if(nums[ind] > nums[i]){
+        //             nge[i] = nums[ind];
+        //             break;
+        //         }
+        //     }
+        // }
+        // return nge;
+
+        // monotonic stack 
+        int n = nums.length; 
         int nge[] = new int[n];
-        Arrays.fill(nge, -1);
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<i+n; j++){
-                int ind = j%n;
-                if(nums[ind] > nums[i]){
-                    nge[i] = nums[ind];
-                    break;
-                }
+        Stack<Integer> st = new Stack<Integer>();
+        for(int i=2*n-1; i>=0; i--){
+            while(!st.empty() && st.peek() <= nums[i%n]){
+                st.pop();
             }
+            if(i<n){
+                nge[i] = st.empty()?-1:st.peek();
+            }
+            st.push(nums[i%n]);
         }
         return nge;
-
     }
 }
