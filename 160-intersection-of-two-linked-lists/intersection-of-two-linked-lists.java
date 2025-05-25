d3783f26dd26c69062cc671cefcb6a6c2ad428dc
Rahul Raj <88604791+rahul-raj58@users.noreply.github.com>
2025-05-25 16:06:19 +0530

    Time: 1 ms (99.88%) | Memory: 48.7 MB (56.87%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -29,35 +29,58 @@ public class Solution {
 
 
         // 2nd approach
-        ListNode t1 = headA;
-        ListNode t2 = headB; 
-        int n1 = 0; 
-        int n2 = 0;
-        while(t1 != null){
-            n1++;
-            t1 = t1.next;
-        }
-        while(t2 != null){
-            n2++;
-            t2 = t2.next;
-        }
-        if(n1 < n2){
-            return collisionPoint(headA,headB,n2-n1);
-        }
-        else{
-            return collisionPoint(headB,headA,n1-n2);
-        }
+    //     ListNode t1 = headA;
+    //     ListNode t2 = headB; 
+    //     int n1 = 0; 
+    //     int n2 = 0;
+    //     while(t1 != null){
+    //         n1++;
+    //         t1 = t1.next;
+    //     }
+    //     while(t2 != null){
+    //         n2++;
+    //         t2 = t2.next;
+    //     }
+    //     if(n1 < n2){
+    //         return collisionPoint(headA,headB,n2-n1);
+    //     }
+    //     else{
+    //         return collisionPoint(headB,headA,n1-n2);
+    //     }
 
-    }
-    public ListNode collisionPoint(ListNode t1, ListNode t2, int diff){
-        while(diff != 0){
-            diff--;
-            t2 = t2.next;
+    // }
+    // public ListNode collisionPoint(ListNode t1, ListNode t2, int diff){
+    //     while(diff != 0){
+    //         diff--;
+    //         t2 = t2.next;
+    //     }
+    //     while(t1 != t2){
+    //         t1 = t1.next;
+    //         t2 = t2.next;
+    //     }
+    //     return t1;
+
+        // 3rd approach
+        if(headA == null || headB == null){
+            return null;
         }
+        ListNode t1 = headA;
+        ListNode t2 = headB;
         while(t1 != t2){
-            t1 = t1.next;
+            t1 = t1.next; 
             t2 = t2.next;
+            if(t1 == t2){
+                return t1;
+            }
+            if(t1 == null){
+                t1 = headB;
+            }
+            if(t2 == null){
+                t2 = headA;
+            }
         }
         return t1;
+
+
     }
 }
