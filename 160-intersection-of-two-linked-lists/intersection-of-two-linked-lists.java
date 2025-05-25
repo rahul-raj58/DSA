18d3772a2b7efe3377fa77f706ce33fb77c70464
Rahul Raj <88604791+rahul-raj58@users.noreply.github.com>
2025-05-25 11:22:39 +0530

    Time: 1 ms (99.88%) | Memory: 49.5 MB (9.28%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -11,20 +11,53 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        HashMap<ListNode, Integer> map = new HashMap<>();
-        ListNode temp1 = headA;
+    //     HashMap<ListNode, Integer> map = new HashMap<>();
+    //     ListNode temp1 = headA;
         
-        while(temp1 != null){
-            map.put(temp1,1);
-            temp1 = temp1.next;
+    //     while(temp1 != null){
+    //         map.put(temp1,1);
+    //         temp1 = temp1.next;
+    //     }
+    //     ListNode temp2 = headB;
+    //     while(temp2 != null){
+    //         if(map.containsKey(temp2)){
+    //             return temp2;
+    //         }
+    //         temp2 = temp2.next;
+    //     }
+    //     return null;
+
+
+        // 2nd approach
+        ListNode t1 = headA;
+        ListNode t2 = headB; 
+        int n1 = 0; 
+        int n2 = 0;
+        while(t1 != null){
+            n1++;
+            t1 = t1.next;
         }
-        ListNode temp2 = headB;
-        while(temp2 != null){
-            if(map.containsKey(temp2)){
-                return temp2;
-            }
-            temp2 = temp2.next;
+        while(t2 != null){
+            n2++;
+            t2 = t2.next;
         }
-        return null;
+        if(n1 < n2){
+            return collisionPoint(headA,headB,n2-n1);
+        }
+        else{
+            return collisionPoint(headB,headA,n1-n2);
+        }
+
+    }
+    public ListNode collisionPoint(ListNode t1, ListNode t2, int diff){
+        while(diff != 0){
+            diff--;
+            t2 = t2.next;
+        }
+        while(t1 != t2){
+            t1 = t1.next;
+            t2 = t2.next;
+        }
+        return t1;
     }
 }
