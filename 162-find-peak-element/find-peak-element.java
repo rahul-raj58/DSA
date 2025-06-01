beec11b79862c518996499bfba7ec75b5ec41c4a
Rahul Raj <88604791+rahul-raj58@users.noreply.github.com>
2025-06-01 11:19:34 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (7.30%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,9 +1,40 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int n = nums.length;
-        for(int i=0; i<nums.length; i++){
-            if((i == 0 || nums[i-1] < nums[i]) && (i == n-1 || nums[i] > nums[i+1])){
-                return i;
+        // brute force 
+        // int n = nums.length;
+        // for(int i=0; i<nums.length; i++){
+        //     if((i == 0 || nums[i-1] < nums[i]) && (i == n-1 || nums[i] > nums[i+1])){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+        // binary search 
+        int n = nums.length; 
+        if(n==1){
+            return 0; 
+
+        }
+        if(nums[0]>nums[1]){
+            return 0;
+        }
+        if(nums[n-1]>nums[n-2]){
+            return n-1;
+        }
+        int low = 1;
+        int high = n-2;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1]){
+                return mid;
+            }
+            // increasing curve
+            if(nums[mid] > nums[mid-1]){
+                low = mid+1;
+            }
+            // decreasing curve
+            else{
+                high = mid-1;
             }
         }
         return -1;
