d359129ed057c231d0b8e1ef447b97fb0a94e337
Rahul Raj <88604791+rahul-raj58@users.noreply.github.com>
2025-06-17 11:48:44 +0530

    Time: 461 ms (5.01%) | Memory: 49.9 MB (62.66%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int n = nums.length;
+        List<Integer> list = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            int count = 0;
+            if(list.contains(nums[i])) continue;
+            for(int j=i; j<n; j++){
+                if(nums[j] == nums[i]){
+                    count++;
+                }
+            }
+            if(count > n/3){
+                list.add(nums[i]);
+            }
+        }
+        return list;
+    }
+}
