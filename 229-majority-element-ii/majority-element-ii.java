39fd0f3845c204166ad6e8b2b018b39f8af428cc
Rahul Raj <88604791+rahul-raj58@users.noreply.github.com>
2025-06-21 15:37:02 +0530

    Time: 13 ms (39.33%) | Memory: 49.4 MB (75.38%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,20 +1,42 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n = nums.length;
+        // Brute
+
+        // List<Integer> list = new ArrayList<>();
+        // for(int i=0; i<n; i++){
+        //     int count = 0;
+        //     if(list.contains(nums[i])) continue;
+        //     for(int j=i; j<n; j++){
+        //         if(nums[j] == nums[i]){
+        //             count++;
+        //         }
+        //     }
+        //     if(count > n/3){
+        //         list.add(nums[i]);
+        //     }
+        //     if (list.size() == 2) break;
+        // }
+        // return list;
+
+        // Hashing 
+        HashMap<Integer, Integer> map = new HashMap<>();
         List<Integer> list = new ArrayList<>();
-        for(int i=0; i<n; i++){
-            int count = 0;
-            if(list.contains(nums[i])) continue;
-            for(int j=i; j<n; j++){
-                if(nums[j] == nums[i]){
-                    count++;
-                }
+        for(int i: nums){
+            if(map.containsKey(i)){
+                map.put(i,map.get(i) + 1);
             }
-            if(count > n/3){
-                list.add(nums[i]);
+            else{
+                map.put(i,1);
             }
-            if (list.size() == 2) break;
+        }
+        for(Map.Entry<Integer,Integer> it: map.entrySet()){
+            if(it.getValue() > n/3){
+                list.add(it.getKey());
+            }
         }
         return list;
+
+
     }
 }
