d260493115e884d4fc2ca27034cb19986d0f76a2
Rahul Raj <88604791+rahul-raj58@users.noreply.github.com>
2026-05-03 09:50:04 +0530

    Time: 14 ms (69.55%) | Memory: 49.1 MB (47.44%) - LeetSync

---
+++ 542-01-matrix/01-matrix.java
@@ -0,0 +1,50 @@
+class Node{
+    int first;
+    int second;
+    int dist;
+    Node(int _first, int _second, int _dist){
+        this.first = _first;
+        this.second = _second;
+        this.dist = _dist;
+    }
+}
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+        int vis[][] = new int[n][m];
+        int dist[][] = new int[n][m];
+        Queue<Node> q = new LinkedList<Node>();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(mat[i][j] == 0){
+                    q.add(new Node(i,j,0));
+                    vis[i][j] = 1;
+                }
+                else{
+                    vis[i][j] = 0;
+                }
+            }
+        }
+
+        int delrow[] = {-1,0,1,0};
+        int delcol[] = {0,1,0,-1};
+        while(!q.isEmpty()){
+            int row = q.peek().first;
+            int col = q.peek().second;
+            int steps = q.peek().dist;
+            q.remove();
+            dist[row][col] = steps;
+            for(int i=0; i<4; i++){
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] == 0){
+                    vis[nrow][ncol] = 1;
+                    q.add(new Node(nrow,ncol,steps+1));
+                }
+            }
+            
+        }
+        return dist;
+    }
+}
