600a5a5fbdde2e696df5fd688a20984475710948
Rahul Raj <88604791+rahul-raj58@users.noreply.github.com>
2026-05-13 14:53:35 +0530

    Time: 21 ms (32.80%) | Memory: 47.6 MB (85.44%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,34 +1,4 @@
-// class Solution {
-//     public int calHours(int [] piles, int banana){
-//         long total = 0;
-//         for(int i: piles){
-//             total += (long)Math.ceil((double)i / banana);
-//         }
-//         return total;
-//     }
-//     public int minEatingSpeed(int[] piles, int h) {
-//         int maxPile = piles[0];
 
-//         for(int i = 1; i < piles.length; i++) {
-//             if(piles[i] > maxPile) {
-//                 maxPile = piles[i];
-//             }
-//         }
-//         int l = 1;
-//         int high = maxPile;
-//         while(l <= high){
-//             int mid = (l+high)/2;
-//             int totalHour = calHours(piles,mid);
-//             if(totalHour <= h){
-//                 high = mid-1;
-//             }
-//             else{
-//                 l = mid+1;
-//             }
-//         }
-//         return l;
-//     }
-// }
 
 class Solution {
 
